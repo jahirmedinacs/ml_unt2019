04014688063a1f721e38a3fbcaadfcc7ce7435ca
Jahir Medina <jahir.medina@pm.me>
2019-10-09 01:40:35 -0500

    v0.00.00.9999

---
+++ Class-Jahir/visual.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+wget https://raw.githubusercontent.com/jahirmedinacs/datamining-p1/master/Practica1.ipynb
+wget https://raw.githubusercontent.com/jahirmedinacs/datamining-p2/master/Practica2.ipynb
+wget https://raw.githubusercontent.com/jahirmedinacs/datamining-p3/master/Practica3.ipynb
+wget https://raw.githubusercontent.com/jahirmedinacs/datamining-p4/master/Practica4.ipynb
+wget https://raw.githubusercontent.com/jahirmedinacs/datamining-p5/master/Practica5.ipynb
+wget https://raw.githubusercontent.com/jahirmedinacs/datamining-p6/master/Practica6.ipynb
+wget https://raw.githubusercontent.com/jahirmedinacs/datamining-p7/master/Practica7.ipynb
